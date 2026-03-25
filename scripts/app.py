f346251cd8e09bb5154810baa97e9f4a6be6902b
Mohab-Haytham <worksmartandhard2@gmail.com>
2026-03-25 14:24:31 +0200

    Added the app.py, dockerfile_flask and the build_flask.yaml

---
+++ scripts/app.py
@@ -0,0 +1,11 @@
+from flask import flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    return "Hellow from Alexandria! My Flask App is running in a container."
+
+
+if __name__== '__main__':
+    app.run(host='0.0.0.0', port=5000)
